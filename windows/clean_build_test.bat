985b0547bd0446aeea625ccecc0eaebd0942874e
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2017-10-31 14:44:50 +1300

    Add Windows script

---
+++ windows/clean_build_test.bat
@@ -0,0 +1,12 @@
+rmdir /s /q xgboost
+git clone %1 --recursive
+cd xgboost
+git checkout %2
+mkdir build
+cd build
+cmake .. -G"Visual Studio 14 2015 Win64" -DUSE_CUDA=ON -DUSE_NCCL=ON -DGPU_COMPUTE_VER="61" -DGOOGLE_TEST=ON
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" xgboost.sln /p:Configuration=Release
+cd ..
+set PYTHONPATH=%cd%\python-package
+python -m nose --attr=!slow tests/python-gpu/
+testxgboost
