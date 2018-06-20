97e875967f810b1e7bb389ffbac3955a97ab5797
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2018-06-20 23:41:52 +0000

    Remove avx

--- ubuntu/clean_build_test.sh
+++ ubuntu/clean_build_test.sh
@@ -19,7 +19,7 @@ fi
 sh ../build_gtest.sh
 #cmake
 mkdir build && cd build
-cmake .. -DUSE_CUDA=ON -DUSE_NCCL=ON -DUSE_AVX=ON -DGOOGLE_TEST=ON -DGTEST_ROOT=../gtest/
+cmake .. -DUSE_CUDA=ON -DUSE_NCCL=ON -DGOOGLE_TEST=ON -DGTEST_ROOT=../gtest/
 make -j4
 cd ..
 cd python-package/
